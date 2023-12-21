174564e9e794d26c13686d6215eb5523ed0a6cc9
bhumi-rastogi <62699095+bhumi-rastogi@users.noreply.github.com>
2023-12-21 10:11:02 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,30 +1,13 @@
-const NEG_INF = -1000000;
-function createPop(list, Objects) {
-  let currIndex = NEG_INF;
-  let check = false;
-
-  function searchForElements(list, givenObjects) {
-    list.forEach((items, index) => {
-      if (items == givenObjects) {
-        currIndex = index;
-        check = true;
+ function createPop(array ,item){
+    for(let i = 0; i < array.length; i++){
+      if(array[i] == item){
+        return `The item is present and is at index ${i}`
       }
-    });
+      if(i+1 == array.length){
+        return "The item is not present and is at index -1000000"
+      }
+    }
   }
 
-  return function () {
-    searchForElements(list, Objects);
-    let index = currIndex;
-    let iPresent = check;
-    if (iPresent) {
-      return `The item is present and is at index ${index}`;
-    } else {
-      return `The items is not present and is at index ${index}`;
-    }
-    };
-}
-
-const arrayofnumbers = [1,2,3,4,5,6];
-const itemstosearch = 4;
-const pop = createPop(arrayofnumbers, itemstosearch);
-pop();
+const arrayOfNumbers = [1,2,3,4,5,6];
+const itemToSearch = 4;
