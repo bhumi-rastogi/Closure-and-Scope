65dd8b3f710612d25803b038ff97dba26aec47e9
bhumi-rastogi <62699095+bhumi-rastogi@users.noreply.github.com>
2023-12-21 09:04:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,29 +1,31 @@
-// Progression 1: create a consant "NEG_INF" with -1000000 value, and a function createPop
 const NEG_INF = -1000000;
-  
-// Progression 2: add 3 variables: title, currIndex, check
-var check = false;
-var currIndex = -1;
+function createPop(x,y) {
 
-function createPop(x,y){
-  for(let i=0; i < x.length;i++ ){
-    if(x[i]==y){
-      check=true;
-      currIndex=i;
-    }
+  let currIndex = NEG_INF;
+  let check = false;
 
+  function searchForElement(x,z){
+    x.forEach(a,b) => {
+    if ((a == z) {
+      currIndex = b;
+      check = true;
+     }
+   });   
   }
 
-    // Progression 3: create a function "searchForElement" 
-   if(!check){
-    return "The item is not present and is at index -1000000";
-   }
-    // Progression 4: In `createPop()`, return a function - which uses the updated values of `currIndex` and `check`
-    // to return the desired output.
-   else{
-    return `The item is present and is at index ${currIndex}`;
-   }
-} 
-const xOfys = [1,2,3,4,5,6];
-const itemToSearch = 4;
+  return function() {
+    searchForElement(x,y)
+    let b = currIndex;
+    let iPresent = check;
+    if (iPresent) {
+      return `The item is present here and lies on index $(Index)`;
+       } else { 
+       return `The item is absent here and lies on index $(Index)`;
+      }
+  };
+}
 
+const arrayOfNumbers = [1,2,3,4,5,6];
+const itemsToSearch = 4;
+const pop = createPop(arrayOfNumbers, itemsToSearch);
+pop();
